b13fd0726891747742d6fcd0c3c7aa9c17d7b4ad
Daniel Alisi <alisidaniel@gmail.com>
2022-06-13 04:02:11 +0200

    chore:proj refactore

--- contracts/Melody.sol
+++ contracts/Melody.sol
@@ -3,11 +3,11 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract MuzikToken is ERC20 {
+contract MelodyToken is ERC20 {
 
     address public admin;
 
-    constructor() ERC20("Melody Coin", "Muzik") {
+    constructor() ERC20("Melody Coin", "MelodyToken") {
         _mint(msg.sender, 1000 * 10 ** 18);
         admin = msg.sender;
     }
