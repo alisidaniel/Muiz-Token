c02e4ece24fede55e0975e1b59cfdd8cf03e55f7
Daniel Alisi <alisidaniel@gmail.com>
2022-06-14 14:40:49 +0200

    test

--- test/melodyTokenTest.ts
+++ test/melodyTokenTest.ts
@@ -3,7 +3,7 @@ import { ethers } from "hardhat";
 
 describe("MelodyToken Contract", function () {
   it("Deployment should assign the total supply of tokens to the owner", async function () {
-    const [owner] = await ethers.getSigners();
+    const [owner, addr1] = await ethers.getSigners();
     const MelodyToken = await ethers.getContractFactory("MelodyToken");
     const melodyToken = await MelodyToken.deploy();
     await melodyToken.deployed();
@@ -11,5 +11,8 @@ describe("MelodyToken Contract", function () {
     const ownerBalance = await melodyToken.balanceOf(owner.address);
 
     expect(await melodyToken.totalSupply()).to.equal(ownerBalance);
+
+    const resTx = await melodyToken.mint(owner.address, 100);
+    expect(await resTx.from).to.eql(owner.address);
   });
 });
