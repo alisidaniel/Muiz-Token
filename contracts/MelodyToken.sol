d7d904b1ae2155a5d45800b415c0f7bbfc7416fc
Daniel Alisi <alisidaniel@gmail.com>
2022-06-14 13:24:38 +0200

    chore:unit test

--- contracts/MelodyToken.sol
+++ contracts/MelodyToken.sol
@@ -1,14 +1,14 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MelodyToken is ERC20 {
-
     address public admin;
 
     constructor() ERC20("Melody Coin", "MelodyToken") {
-        _mint(msg.sender, 1000 * 10 ** 18);
+        _mint(msg.sender, 1000 * 10**18);
         admin = msg.sender;
     }
 
@@ -17,7 +17,7 @@ contract MelodyToken is ERC20 {
         _mint(to, amount);
     }
 
-    function burn(uint amount) external {
+    function burn(uint256 amount) external {
         _burn(msg.sender, amount);
     }
 }
--- test/myTokenTest.ts
+++ test/myTokenTest.ts
@@ -1,21 +1,36 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
+let owner: any;
+let contract: any;
+let addr1: any;
+let addr2: any;
+
+const token = "Aliwave";
+const symbol = "ALW";
+const decimals = 18;
+const totalSupply = "1000000000000000000000";
+const amount = 1000;
+
 describe("MyToken Contract", function () {
   it("Deployment should assign the total supply of tokens to the owner", async function () {
-    const [owner, addr1, addr2] = await ethers.getSigners();
-    const MyToken = await ethers.getContractFactory("MelodyToken");
-    const myToken = await MyToken
-      .deploy
-      //   "Aliwave",
-      //   "ALW",
-      //   "18",
-      //   "100000000000000000000000"
-      ();
-    await myToken.deployed();
+    [owner, addr1, addr2] = await ethers.getSigners();
 
-    const ownerBalance = await myToken.balanceOf(owner.address);
+    const TokenContract = await ethers.getContractFactory("MyToken");
+    contract = await TokenContract.deploy(token, symbol, decimals, totalSupply);
+    await contract.deployed();
 
-    expect(await myToken.totalSupply()).to.equal(ownerBalance);
+    const ownerBalance = await contract.balanceOf(owner.address);
+
+    expect(await contract.totalSupply()).to.equal(ownerBalance);
+    expect(await contract.name()).to.equal(token);
+    expect(await contract.symbol()).to.equal(symbol);
+    expect(await contract.decimals()).to.equal(decimals);
+  });
+
+  it("It should transfer token to address and not throw error", async function () {
+    await contract.transfer(addr2.address, amount);
+    const currentBalance = await contract.balanceOf(addr2.address);
+    expect(+currentBalance.toString()).to.equal(amount);
   });
 });
